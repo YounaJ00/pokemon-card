10c57dc3e44b97ad9747c129927c5a0c6dfe96d0
younaj00 <you.na.joo0@gmail.com>
2025-03-13 11:52:54 +0900

    feat: Created PokeCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,57 @@
-import { useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'; 
+import PokeCard from './components/PokeCard';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // 4
+  const [pokemons, setPokemons] = useState([]);
 
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">hello world</h1>
-    </div>
+  // 3
+  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset'
+
+  useEffect(() => {  // 2
+    fetchPokemonsData();
+  }, [])
+  
+  const fetchPokemonsData = async() => {
+    try { // 1
+      const response = await axios.get(url); 
+      console.log(response.data.results);
+      setPokemons(response.data.results); 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return ( 
+    <article className='pt-6'>
+      <header className='flex flex-col gap-2 w-full px-4 z-50'>
+        헤더 inpute form
+      </header>
+      <section className='pt-6 flex flex-col justify-center items-center'>
+        <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl'>
+
+          {pokemons.length > 0 ? 
+          (
+            pokemons.map(({url, name}, index) => (
+                <PokeCard key={url} url={url} name={name}/>
+            ))
+          ) : 
+          (
+            <h2 className='font-medium text-lg text-slate-900 mb-1'>
+              포켓몬이 없습니다!
+            </h2>
+          )}
+          
+          
+        </div>
+      </section>
+    </article>
   )
 }
 
