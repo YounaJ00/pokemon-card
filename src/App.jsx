1992ebd132f184379e19fe4bd326c30f9d959d97
younaj00 <you.na.joo0@gmail.com>
2025-03-22 14:15:05 +0900

    feat: AutoComplete 컴포넌트 생성하기

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import axios from 'axios'; 
 import PokeCard from './components/PokeCard';
 import { useDebounce } from './hooks/useDebounce';
+import AutoComplete from './components/AutoComplete';
 
 
 function App() {
@@ -20,19 +21,15 @@ function App() {
   const limitNum = 20; 
   const url = `https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0`;
 
-  // c. 검색기능
-  const [searchTerm, setSearchTerm] = useState("");
-  // d. 디바운싱 hooks 가져오기
-  const debouncedSearchTerm = useDebounce(searchTerm, 250);
+  // c. 검색기능 - autocomplete 으로 감
+   const [searchTerm, setSearchTerm] = useState("");
+
 
   useEffect(() => {  // a
     fetchPokeData();
   }, [])
 
-  useEffect(() => {
-    handleSearchInput(debouncedSearchTerm)
-  }, [debouncedSearchTerm])
-  
+
   const filterDisplayedPokemonData = (allPokemonData, displayedPokemons = []) => {
     const limit = displayedPokemons.length + limitNum;
     // 모든 포켓몬 데이터에서 limitNum 만큼 더 가져오기
@@ -54,47 +51,17 @@ function App() {
     }
   }
 
-  const handleSearchInput = async (searchTerm) => {
-    
-    if(searchTerm.length > 0) {
-      try {
-          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
-          const pokemonData = {
-            url: `https://pokeapi.co/api/v2/pokemon/${response.data.id}`,
-            name: searchTerm
-          }
-          setAllpokemons([pokemonData])
-      } catch (error) {
-        setAllpokemons([]);
-        console.error(error);
-      }
-    }else {
-      fetchPokeData(true);
-    }
-  }
 
 
 
   return ( 
     <article className='pt-6'>
       <header className='flex flex-col gap-2 w-full px-4 z-50'>
-        <div className='relative z-50'>
-        <form 
-          className='relative flex justify-center items-center w-[20.5rem] h-6 rounded-lg m-auto'>
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className='text-xs w-[20.5rem] h-6 px-2 py-1 bg-slate-900 bg-opacity-50 rounded-lg text-gray-200 text-center' 
-            />
-            
-            <button
-              type='submit'
-              className='text-xs bg-slate-700 text-slate-300 w-[2.5rem] h-6 px-2 py-1 rounded-r-lg text-center absolute right-0 hover:bg-slate-400'>
-              검색
-            </button>
-        </form>
-        </div>
+        <AutoComplete 
+          allPokemons={allPokemons}
+          setDisplayedPokemons={setDisplayedPokemons}
+          searchTerm={searchTerm}  
+          setSearchTerm={setSearchTerm} />
       </header>
       <section className='pt-6 flex flex-col justify-center items-center'>
         <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl'>
@@ -121,6 +88,7 @@ function App() {
                 More
               </button>) 
           }
+         
          
       </div>
     </article>
