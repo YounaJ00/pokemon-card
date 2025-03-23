cc855cbf69bc9c2638a96d18b3189aea735b8fed
younaj00 <you.na.joo0@gmail.com>
2025-03-24 00:25:20 +0900

    feat: 데이터 가공

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const DetailPage = () => {
+  
+  const [pokemon, setPokemon] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   
   const params = useParams();
   const pokemonId = params.id;
@@ -20,20 +23,64 @@ const DetailPage = () => {
       
       if(pokemonData) {
         const {name, id, types, weight, height, stats, abilities} = pokemonData;
-        console.log(id);
         const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id);
-        console.log(nextAndPreviousPokemon)
+ 
+        const DamageRelations = await Promise.all(
+          types.map(async(i) => {
+            const type = await axios.get(i.type.url);
+            return type.data.damage_relations
+          })
+        )
+
+        
+        const formattedPokemonData = {
+          id,
+          name,
+          weight: weight/10,
+          height: height/10,
+          previous: nextAndPreviousPokemon.previous,
+          next: nextAndPreviousPokemon.next,
+          abilities: formatPokemonAbilities(abilities),
+          stats: formatPokemonStats(stats),
+          DamageRelations
+        }
+
+
+        setPokemon(formattedPokemonData);
+        setIsLoading(false);
       }
+
 
     } catch (error) {
       console.log(error);
     }
   }
 
+  const formatPokemonStats = ([
+    statHP,
+    statATK,
+    statDEP,
+    statSATK,
+    statSDEP,
+    statSPD
+  ]) => [
+    {name: 'Hit Points', baseStat: statHP.base_stat},
+    {name: 'Attack', baseStat: statATK.base_stat},
+    {name: 'Defense', baseStat: statDEP.base_stat},
+    {name: 'Special Attack', baseStat: statSATK.base_stat},
+    {name: 'Special Defense', baseStat: statSDEP.base_stat},
+    {name: 'Speed', baseStat: statSPD.base_stat}
+  ]
+
+  const formatPokemonAbilities = (abilities) => {
+    return abilities.filter((_, index) => index <= 1)
+                          .map((obj) => obj.ability.name.replaceAll('-', ' ') )
+  }
+
   async function getNextAndPreviousPokemon(id) {
     const urlPokemon = `${baseUrl}?limit=1&offset=${id-1}`;
     const {data: pokemonData} = await axios.get(urlPokemon);
-    console.log('*****',pokemonData)
+    
 
     const nextResponse = pokemonData.next && (await axios.get(pokemonData.next))
     const previousResponse = pokemonData.previous && (await axios.get(pokemonData.previous))
@@ -45,6 +92,8 @@ const DetailPage = () => {
       previous: previousResponse?.data?.results?.[0]?.name
     }
   }
+
+  if(isLoading) return <div>...Loading</div>
 
   return (
     <div> detailPage이다</div>
